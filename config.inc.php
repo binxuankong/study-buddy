e8401fe18ddccfc8049301189cd5a49d8f2a6761
mbax4ej6 <ellis.judge1@me.com>
2016-02-03 09:25:02 +0000

    Added config.inc.php template

---
+++ config.inc.php
@@ -0,0 +1,8 @@
+<?php
+//This is the configuration file for database access.
+//DO NOT COMMIT ANY CHANGES TO THIS FILE TO GIT
+$database_host = "dbhost.cs.man.ac.uk";
+$database_user = "<YOUR USERNAME>";
+$database_pass = "<YOUR DATABASE PASSWORD>";
+$group_dbnames = array("2015_comp10120_m3",);
+?>
