964c9e5ca8f1a0dfd5ebce6aebc8d26b8371adfc
Yangguang <lyangguan@gmail.com>
2016-02-16 17:33:48 +0000

    resolve conflict

--- Pages/CreateModule.php
+++ Pages/CreateModule.php
@@ -36,25 +36,19 @@
           <div class="col-md-10">
 
           <?php    
-          if(isset($_POST['SubmitButton'])){ //check if form was submitted
-            $input = $_POST['inputText']; //get input text
-            echo "Success! You entered: ".$input;
-          } // if  
           ?>
 	      
           <form action="">
 	        <p>         
           Module Code:
-          <input type="text" name="code" value="e.g. COMP16121">
+          <input type="text" name="code" placeholder="e.g. COMP16121">
           <br><br>
           Module Name:
           <input type="text" name="name" size="50"
-          value="e.g. Object Orientated Programming with Java">
+          placeholder="e.g. Object Orientated Programming with Java">
           <br><br>
           Module Description:<br>
-          <textarea name="description" value="e.g. First Year Java Course 
-          for Computer Science" rows="4" cols="63">
-          </textarea>
+          <textarea name="description" placeholder="e.g. First Year Java Course for Computer Science" rows="4" cols="63"></textarea>
           <br><br><br>
           <input type="submit" value="Submit Module">
           </p>
