d328e1b0debdbfe73cab27b1c13e1be6e0e3e0f7
Bin Xuan Kong <bin.kong@student.manchester.ac.uk>
2016-02-16 13:44:56 +0000

    Replace report a bug

--- Pages/SubmitQuestion.php
+++ Pages/SubmitQuestion.php
@@ -90,9 +90,9 @@
             <p><a href="#">Get going with Study Buddy</a></p>
           </div>
           <div class="col-md-3">
-            <h3>Solutions</h3>
-            <p>Contact us if you encounter any bugs or problems and let us solve your problems.</p>
-            <p><a href="Feedback.html">Report a bug</a></p>
+            <h3>Feedback</h3>
+            <p>Contact us if you encounter any problems or if you have any suggestions to improve our website and let us solve your problems.</p>
+            <p><a href="Feedback.html">Send a feedback</a></p>
           </div>
         </div>
       </div>
