c3082976a615c7975b698928c6e11b70202e45f6
Ellis Judge <ellis.judge@student.cs.man.ac.uk>
2016-02-17 10:23:48 +0000

    In theory it will tick

--- Pages/TimerScript.js
+++ Pages/TimerScript.js
@@ -1,7 +1,6 @@
 // <button id="Start-Stop" onclick="clickButton(3000);">Start</button> <!--Time is in millisecond-->
-
 var timer;
-var start;
+var start = false;
 var time = 600;
 
 function increaseTime()
@@ -23,6 +22,10 @@ function tick()
 {
   time = time - 1;
   displayTime();
+  if(time == 0)
+  {
+    openWindow();
+  }
 }
 
 function displayTime()
@@ -37,64 +40,28 @@ function displayTime()
 }
 // Change between start and stop button when user click it.
 // Stop timer if user click stop.
-function clickButton(time)
+function clickButton(timeSet)
 {
+  time = timeSet;
   if (!start)
   {
     document.getElementById("Start-Stop").innerHTML = "Stop";
     start = true;
-    popup(time);
+    timer = setInterval(tick, 1000);
   }
   else
   {
     document.getElementById("Start-Stop").innerHTML = "Start";
     start = false;
-    clearTimeout(timer);
+    clearInterval(timer);
   }
 } // clickButton
 
-
-// Open a window after given time.
-function popup(time) 
-{
-var mylink = "ExercisePage.php";
-var windowname = "Questions";
-
-timer = setTimeout(openwindow,time,mylink,windowname);
-} 
-
-
 // Open a window containing exercise page.
-function openwindow(mylink, windowname)
+function openwindow()
 {
+  var mylink = "ExercisePage.php";
+  var windowname = "Questions";
   var myWindow = window.open(mylink, windowname, "type=fullwindow,fullscreen=yes,height=screen.availHeight,width=screen.availWidth,left=0,top=0,resizeable=no");
   myWindow.focus();
-}
-
-//----------------------------------------------------------------------------//
-// Check if user has answered all questions.
-// If not, pop up exercise window in 1 ms.
-function confirmExit(time) {
-var form = document.getElementById("frml");
-var full = true;
-
-for (var i = 0; i < form.length; i++) {
-  if (form[i] == "") {full = false;};
-};
-
-if (full) {
-  popup(time);
-  return true;
-} // if
-else {
-  if(confirm('You attempt to leave the page. Have you finish all questions?'))
-  {
-    popup(1);
-    return true;
-  }
-  else
-  {
-    return false;
-  }
-}
 }
