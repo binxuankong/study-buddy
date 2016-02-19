c056f0e13aa7c79e5fd13af76203a96ed72aa7a4
Ellis Judge <ellis.judge1@me.com>
2016-02-19 08:13:17 +0000

    modified shuffle function

--- Pages/randomizer.php
+++ Pages/randomizer.php
@@ -1,15 +1,20 @@
 <?php
 
 // PHP code to randomize the answer choices.
-function shuffle_assoc($list) {
-    if (!is_array($list)) return $list;
-    $keys = array_keys($list);
-    shuffle($keys);
-    $random = array();
-    foreach ($keys as $key) {
-        $random[$key] = $list[$key];
-    }
-    return $random;
+function shuffle_array($list) 
+{
+  if (!is_array($list)) 
+  {
+    return $list;
   }
+  $keys = array_keys($list);
+  shuffle($keys);
+  $random = array();
+  foreach ($keys as $key) 
+  {
+    $random[$key] = $list[$key];
+  }
+  return $random;
+}
 
 ?>
