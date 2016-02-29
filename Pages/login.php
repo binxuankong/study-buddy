803ab50cc05c6e7418db8a93322ee0c139695624
Ellis Judge <ellis.judge1@me.com>
2016-02-29 19:09:35 +0000

    80 char/line rule

--- Pages/login.php
+++ Pages/login.php
@@ -10,11 +10,13 @@
     <div class="nav">
       <div class="container">
         <ul class="pull-left">
-           <a href="../index.html"><img src="../Images/new_logo.png" alt="Study Buddy">
+           <a href="../index.html"><img src="../Images/new_logo.png" 
+                                    alt="Study Buddy">
            <li id="webpagename">Study Buddy</li></a>
         </ul>
         <ul class="pull-right">
-          <li><a href="#"><img src="../Images/new_user.png" alt="User Profile"></a></li>
+          <li><a href="#"><img src="../Images/new_user.png" 
+                           alt="User Profile"></a></li>
           <li id="signup"><a href="#">Sign Up/Log In</a></li>
         </ul>
       </div>
@@ -31,11 +33,13 @@
         <?php
           //create database connection object
           require_once('../config.inc.php');
-          $mysqli = new mysqli($database_host, $database_user, $database_pass, $database_name);
+          $mysqli = new mysqli($database_host, $database_user, 
+                               $database_pass, $database_name);
           //check database connection
           if($mysqli -> connect_error) 
           {
-            die('Connect Error ('.$mysqli -> connect_errno.') '.$mysqli -> connect_error);
+            die('Connect Error ('.$mysqli -> connect_errno.') '
+                .$mysqli -> connect_error);
           }
           //if the user clicked the sign up button
           if(isset($_POST['register']))
@@ -47,7 +51,8 @@
                     ."Email address:<br><input name='email' type='email'><br>"
                     ."Username:<br><input name='username' type='text'><br>"
                     ."Password:<br><input name='password' type='password'><br>"
-                    ."Confirm password:<br><input name='passwordConfirm' type='password'><br>"
+                    ."Confirm password:<br><input name='passwordConfirm' "
+                    ."type='password'><br>"
                     ."<input name='registered' type='submit' value='Register'>"
                   ."</form>";
           }
@@ -55,8 +60,9 @@
           else if(isset($_POST['registered']))
           {
             //check all values were entered
-            if(isset($_POST['firstName']) && $_POST['surname'] && $_POST['email']
-               && $_POST['username'] && $_POST['password'] && $_POST['passwordConfirm'])
+            if(isset($_POST['firstName']) && isset($_POST['surname']) 
+               && isset($_POST['email']) && isset($_POST['username'])
+               && isset($_POST['password']) && isset($_POST['passwordConfirm']))
             {
               //store the form values for use.
               $firstName = $_POST['firstName'];
@@ -79,17 +85,22 @@
                 $passwordHash = hash("sha512", $saltedPasswordPreHash);
                 
                 //insert all user info
-                $query = "INSERT INTO SB_USER_INFO (userFirstName, userSurname, userEmail,  userScreenName) VALUES ('$firstName', '$surname', '$email', '$username')";
+                $query = "INSERT INTO SB_USER_INFO (userFirstName, userSurname,"
+                         ." userEmail,  userScreenName) VALUES ('$firstName', "
+                         ."'$surname', '$email', '$username')";
                 if ($mysqli->query($query) === TRUE) 
                 {
-                  echo "New record created successfully";
+                  echo "New user created successfully";
                 }
                 else 
                 {
-                  echo "Error: " . $query . "<br>" . $mysqli->error;
+                  die('Connect Error ('.$mysqli -> connect_errno.') '
+                      .$mysqli -> connect_error);
                 }
                 //retrieve the userID
-                $result = $mysqli -> query("SELECT userID FROM SB_USER_INFO WHERE userScreenName='$username'");
+                $query = "SELECT userID FROM SB_USER_INFO WHERE "
+                         ."userScreenName='$username'";
+                $result = $mysqli -> query();
                 //check that a user exists
                 if($result -> num_rows == 1)
                 {
@@ -97,24 +108,114 @@
                   $userIDArray = $result -> fetch_assoc();
                   $userID = $userIDArray['userID'];
                   //insert the hashed password and the salt
-                  $query = "INSERT INTO SB_LOGIN_CREDENTIALS (userID, userPasswordHash, userSalt) VALUES ('$userID', '$passwordHash', '$hexedSalt')";
+                  $query = "INSERT INTO SB_LOGIN_CREDENTIALS (userID, "
+                           ."userPasswordHash, userSalt) VALUES ('$userID', "
+                           ."'$passwordHash', '$hexedSalt')";
                   if ($mysqli->query($query) === TRUE) 
                   {
-                    echo "New record created successfully";
+                    echo "New credentials created successfully";
                   }
                   else 
                   {
-                    echo "Error: " . $query . "<br>" . $mysqli->error;
+                    die('Connect Error ('.$mysqli -> connect_errno.') '
+                        .$mysqli -> connect_error);
                   }
                 }
-                //something went wrong
+                //password does not match confirmed password
                 else
-                {}
+                {
+                  //reproduce form with missing items from registration form.
+              
+                  //set variables to blank
+                  $firstName = "";
+                  $surname = "";
+                  $email = "";
+                  $username = "";
+                  $password = "";
+                  $passwordConfirm = "";
+                  
+                  //get entered values
+                  if(isset($_POST['firstName']))
+                  {
+                    $firstName = $_POST['firstName'];
+                  }
+                  if(isset($_POST['surname']))
+                  {
+                    $surname = $_POST['surname'];
+                  }
+                  if(isset($_POST['email']))
+                  {
+                    $email = $_POST['email'];
+                  }
+                  if(isset($_POST['username']))
+                  {
+                    $username = $_POST['username'];
+                  }
+                  //echo error message and display the form
+                  echo "Password does not match the password confirmation <br>";
+                  echo "<form method='post'>"
+                        ."First name:<br><input name='firstName' type='text' "
+                        ."value='$firstName'><br>"
+                        ."Surname:<br><input name='surname' type='text' "
+                        ."value='$surname'><br>"
+                        ."Email address:<br><input name='email' type='email' "
+                        ."value='$email'><br>"
+                        ."Username:<br><input name='username' type='text' "
+                        ."value='$username'><br>"
+                        ."Password:<br><input name='password' type='password'>"
+                        ."<br>"
+                        ."Confirm password:<br><input name='passwordConfirm' "
+                        ."type='password'><br>"
+                        ."<input name='registered' type='submit' "
+                        ."value='Register'>"
+                      ."</form>";
+                }
               }
             }
             else
             {
               //reproduce form with missing items from registration form.
+              
+              //set variables to blank
+              $firstName = "";
+              $surname = "";
+              $email = "";
+              $username = "";
+              $password = "";
+              $passwordConfirm = "";
+              
+              //get entered values
+              if(isset($_POST['firstName']))
+              {
+                $firstName = $_POST['firstName'];
+              }
+              if(isset($_POST['surname']))
+              {
+                $surname = $_POST['surname'];
+              }
+              if(isset($_POST['email']))
+              {
+                $email = $_POST['email'];
+              }
+              if(isset($_POST['username']))
+              {
+                $username = $_POST['username'];
+              }
+              //echo the form
+              echo "<form method='post'>"
+                    ."First name:<br><input name='firstName' type='text' "
+                    ."value='$firstName'><br>"
+                    ."Surname:<br><input name='surname' type='text' "
+                    ."value='$surname'><br>"
+                    ."Email address:<br><input name='email' type='email' "
+                    ."value='$email'><br>"
+                    ."Username:<br><input name='username' type='text' "
+                    ."value='$username'><br>"
+                    ."Password:<br><input name='password' type='password'><br>"
+                    ."Confirm password:<br><input name='passwordConfirm' "
+                    ."type='password'><br>"
+                    ."<input name='registered' type='submit' value='Register'>"
+                  ."</form>";
             }
           }
           //user attempted to login
@@ -130,10 +231,13 @@
               $enteredPassword = $_POST['password'];
               
               //get user credentials from DB
-              $result = $mysqli -> query("SELECT * FROM SB_USER_INFO WHERE userScreenName='$username'");
+              $query = "SELECT * FROM SB_USER_INFO WHERE "
+                        ."userScreenName='$username'";
+              $result = $mysqli -> query($query);
               if($result->num_rows == 0)
               {
                 //incorrect username
+                //do not need to do anything as this will be tested for again
               }
               else if($result->num_rows == 1)
               {
@@ -143,12 +247,16 @@
                 if(strcmp($username, $userIDArray['userScreenName']))
                 {
                   //THIS SHOULD NEVER HAPPEN
-                  echo "<a href='./Feedback.html'>Catastrophic failure: user mismatch. Please click here to report this using the feedback form</a><br>";
+                  echo "<a href='./Feedback.html'>Catastrophic failure: user "
+                       ."mismatch. Please click here to report this using the "
+                       ."feedback form</a><br>";
                 }
                 else
                 {
                   //get the user credentials
-                  $result = $mysqli -> query("SELECT * FROM SB_LOGIN_CREDENTIALS WHERE userID='$userID'");
+                  $query = "SELECT * FROM SB_LOGIN_CREDENTIALS WHERE "
+                           ."userID='$userID'";
+                  $result = $mysqli -> query($query);
                   //user has info but no credentials
                   if($result->num_rows == 0)
                   {
@@ -176,7 +284,8 @@
                     $login = true;
                     //start a session to store login information
                     session_start();
-                    //store the user screen name and the userID to be used elsewhere
+                    //store the user screen name and the userID to be used
+                    //on other pages
                     $_SESSION['userName'] = $username;
                     $_SESSION['userID'] = $userID;
                   }
@@ -186,7 +295,9 @@
               else
               {
                 //THIS SHOULD NEVER EVER HAPPEN
-                echo "<a href='./Feedback.html'>Catastrophic failure: multiple users. Please click here to report this using the feedback form</a>";
+                echo "<a href='./Feedback.html'>Catastrophic failure: multiple "
+                     ."users. Please click here to report this using the "
+                     ."feedback form</a>";
               }
               //if successfully logged in
               if($login)
@@ -200,12 +311,15 @@
                 //redisplay logn form with entered username but not password
                 echo "Invalid username or password";
                 echo "<form method='post'>"
-                      ."Username:<br><input type='text' name='username' value='$username'><br>"
-                      ."<Password<br><input type='password' name='password'><br>"
+                      ."Username:<br><input type='text' name='username' "
+                      ."value='$username'><br>"
+                      ."<Password<br><input type='password' name='password'>"
+                      ."<br>"
                       ."<input type='submit' name='login' value='login'><br>"
                     ."</form>"
                     ."<form method='post'>"
-                      ."<input type='submit' name='register' value='Click Here to sign up'>"
+                      ."<input type='submit' name='register' "
+                      ."value='Click Here to sign up'>"
                     ."</form>";
               }
             }
@@ -215,11 +329,13 @@
               //display standard login form
               echo "<form method='post'>"
                       ."Username:<br><input type='text' name='username'><br>"
-                      ."Password:<br><input type='password' name='password'><br>"
+                      ."Password:<br><input type='password' name='password'>"
+                      ."<br>"
                       ."<input type='submit' name='login' value='login'><br>"
                     ."</form>"
                     ."<form method='post'>"
-                      ."<input type='submit' name='register' value='Click Here to sign up'>"
+                      ."<input type='submit' name='register' "
+                      ."value='Click Here to sign up'>"
                     ."</form>";
             }
           }
@@ -230,21 +346,28 @@
       <div class="container">
         <div class="row">
           <div class="col-md-3">
-             <h2><img src="../Images/new_logo.png" alt="Study Buddy"></img>Study Buddy</h2>
+            <h2>
+              <img src="../Images/new_logo.png" alt="Study Buddy">
+              Study Buddy
+            </h2>
           </div>
           <div class="col-md-3">
             <h3>About Us</h3>
-            <p>The team behind this website is the M3 Group of the School of Computer Science from the University of Manchester.</p>
+            <p>The team behind this website is the M3 Group of the School 
+               of Computer Science from the University of Manchester.</p>
             <p><a href="#">Learn more about each members of the team</a></p>
           </div>
           <div class="col-md-3">
             <h3>Get Started</h3>
-            <p>Stop wasting precious time and come join us now to start your revision.</p>
+            <p>Stop wasting precious time and come join us now to start 
+               your revision.</p>
             <p><a href="#">Get going with Study Buddy</a></p>
           </div>
           <div class="col-md-3">
             <h3>Feedback</h3>
-            <p>Contact us if you encounter any problems or if you have any suggestions to improve our website and let us solve your problems.</p>
+            <p>Contact us if you encounter any problems or if you have any 
+               suggestions to improve our website and let us solve your 
+               problems.</p>
             <p><a href="Feedback.html">Send a feedback</a></p>
           </div>
         </div>
