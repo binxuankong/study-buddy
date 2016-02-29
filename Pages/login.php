161c3a8611ef46c2c409b093beb0d1dfcd2ce867
Ellis Judge <ellis.judge1@me.com>
2016-02-29 16:51:01 +0000

    login page, (sort of) works

--- Pages/login.php
+++ Pages/login.php
@@ -22,125 +22,210 @@
 
     <div class="heading">
       <div class="container">
-        <h1>Template</h1>
+        <h1>Sign up / Login</h1>
       </div>
     </div>
 	      
     <div class="body">
       <div class="container">
         <?php
+          //create database connection object
+          require_once('../config.inc.php');
+          $mysqli = new mysqli($database_host, $database_user, $database_pass, $database_name);
+          //check database connection
+          if($mysqli -> connect_error) 
+          {
+            die('Connect Error ('.$mysqli -> connect_errno.') '.$mysqli -> connect_error);
+          }
+          //if the user clicked the sign up button
           if(isset($_POST['register']))
           {
-            echo "<form method='post'>
-                    <input name='firstName' type='text'><br>
-                    <input name='surname' type='text'><br>
-                    <input name='email' type='email'><br>
-                    <input name='username' type='text'><br>
-                    <input name='password' type='password'><br>
-                    <input name='passwordConfirm' type='password'><br>
-                    <input name='registered' type='submit' value='Register'>
-                  </form>";
+            //display the sign up form
+            echo "<form method='post'>"
+                    ."First name:<br><input name='firstName' type='text'><br>"
+                    ."Surname:<br><input name='surname' type='text'><br>"
+                    ."Email address:<br><input name='email' type='email'><br>"
+                    ."Username:<br><input name='username' type='text'><br>"
+                    ."Password:<br><input name='password' type='password'><br>"
+                    ."Confirm password:<br><input name='passwordConfirm' type='password'><br>"
+                    ."<input name='registered' type='submit' value='Register'>"
+                  ."</form>";
           }
+          //else if the user filled in the registration form
           else if(isset($_POST['registered']))
           {
-            
+            //check all values were entered
+            if(isset($_POST['firstName']) && $_POST['surname'] && $_POST['email']
+               && $_POST['username'] && $_POST['password'] && $_POST['passwordConfirm'])
+            {
+              //store the form values for use.
+              $firstName = $_POST['firstName'];
+              $surname = $_POST['surname'];
+              $email = $_POST['email'];
+              $username = $_POST['username'];
+              $password = $_POST['password'];
+              $passwordConfirm = $_POST['passwordConfirm'];
+              //check if username or email exists in DB
+              //if password matches confirmation password
+              if(strcmp($password, $passwordConfirm) == 0)
+              {
+                //generate the user salt
+                $generatedSalt = openssl_random_pseudo_bytes(32);
+                //convert the salt to text
+                $hexedSalt = bin2hex($generatedSalt);
+                //appent the password to the salt
+                $saltedPasswordPreHash = $hexedSalt.$password;
+                //hash the salted password using sha512
+                $passwordHash = hash("sha512", $saltedPasswordPreHash);
+                
+                //insert all user info
+                $query = "INSERT INTO SB_USER_INFO (userFirstName, userSurname, userEmail,  userScreenName) VALUES ('$firstName', '$surname', '$email', '$username')";
+                if ($mysqli->query($query) === TRUE) 
+                {
+                  echo "New record created successfully";
+                }
+                else 
+                {
+                  echo "Error: " . $query . "<br>" . $mysqli->error;
+                }
+                //retrieve the userID
+                $result = $mysqli -> query("SELECT userID FROM SB_USER_INFO WHERE userScreenName='$username'");
+                //check that a user exists
+                if($result -> num_rows == 1)
+                {
+                  //get the relevant user's ID
+                  $userIDArray = $result -> fetch_assoc();
+                  $userID = $userIDArray['userID'];
+                  //insert the hashed password and the salt
+                  $query = "INSERT INTO SB_LOGIN_CREDENTIALS (userID, userPasswordHash, userSalt) VALUES ('$userID', '$passwordHash', '$hexedSalt')";
+                  if ($mysqli->query($query) === TRUE) 
+                  {
+                    echo "New record created successfully";
+                  }
+                  else 
+                  {
+                    echo "Error: " . $query . "<br>" . $mysqli->error;
+                  }
+                }
+                //something went wrong
+                else
+                {}
+              }
+            }
+            else
+            {
+              //reproduce form with missing items from registration form.
+            }
           }
+          //user attempted to login
           else
           {
             if(isset($_POST['username']) && isset($_POST['password']))
             {
+              //assume login will fail
               $login = false;
-              //create database connection
-              require_once('../config.inc.php');
-              $mysqli = new mysqli($database_host, $database_user, $database_pass, $database_name);
-              //check database connection
-              if($mysqli -> connect_error) 
-              {
-                die('Connect Error ('.$mysqli -> connect_errno.') '.$mysqli -> connect_error);
-              }
               
-              //get username and password
+              //get username and entered password
               $username = $_POST['username'];
-              $password = $_POST['password'];
+              $enteredPassword = $_POST['password'];
               
-              //get usercredentials
+              //get user credentials from DB
               $result = $mysqli -> query("SELECT * FROM SB_USER_INFO WHERE userScreenName='$username'");
               if($result->num_rows == 0)
               {
-                //invalid username
+                //incorrect username
               }
               else if($result->num_rows == 1)
               {
-                $user = $result->fetch_assoc();
+                $userIDArray = $result->fetch_assoc();
+                $userID = $userIDArray['userID'];
                 //username accepted but just to make sure...
-                if($username != $user['userScreenName'];)
+                if(strcmp($username, $userIDArray['userScreenName']))
                 {
                   //THIS SHOULD NEVER HAPPEN
-                  echo "<a href='./Feedback.html'>Catastrophic failure: user mismatch. Please click here to report this using the feedback form</a>"
+                  echo "<a href='./Feedback.html'>Catastrophic failure: user mismatch. Please click here to report this using the feedback form</a><br>";
                 }
                 else
                 {
-                  $passwordHash = password_hash($password);
-                  if($passwordHash == $user['userPasswordHash'])
+                  //get the user credentials
+                  $result = $mysqli -> query("SELECT * FROM SB_LOGIN_CREDENTIALS WHERE userID='$userID'");
+                  //user has info but no credentials
+                  if($result->num_rows == 0)
                   {
+                    //Password not set. fix this.
+                  }
+                  else if($result->num_rows == 1)
+                  {
+                    //store the credentials for use
+                    $userCredentials = $result->fetch_assoc();
+                    $storedSalt = $userCredentials['userSalt'];
+                    $storedPasswordHash = $userCredentials['userPasswordHash'];
+                  }
+                  else
+                  {
+                    //multiple user passwords stored ????
+                  }
+                  //add the password to the retrieved salt
+                  $passwordSalted = $storedSalt.$enteredPassword;
+                  //hash the salted password
+                  $enteredPasswordHash = hash("sha512", $passwordSalted);
+                  //compare the entered passwords hash to the stored hash
+                  if($enteredPasswordHash == $storedPasswordHash)
+                  {
+                    //set login to true to allow the user to login
                     $login = true;
+                    //start a session to store login information
                     session_start();
-                    $_SESSION['userName'] = $user['userScreenName'];
-                    $_SESSION['userID'] = $user['userID'];
+                    //store the user screen name and the userID to be used elsewhere
+                    $_SESSION['userName'] = $username;
+                    $_SESSION['userID'] = $userID;
                   }
                 }
               }
+              //multiple users with the same username
               else
               {
-                //multiple users with the same username
                 //THIS SHOULD NEVER EVER HAPPEN
                 echo "<a href='./Feedback.html'>Catastrophic failure: multiple users. Please click here to report this using the feedback form</a>";
               }
+              //if successfully logged in
               if($login)
               {
                 //redirect user
                 echo "LOGIN SUCCESSFUL";
               }
+              //login failed username or password checks
               else
               {
+                //redisplay logn form with entered username but not password
                 echo "Invalid username or password";
-                echo "
-                    <form method='post'>
-                      <input type='text' name='username' value='$username'><br>
-                      <input type='password' name='password'><br>
-                      <input type='submit' name='login' value='login'>
-                    </form>
-                    <form method='post'>
-                      <input type='submit' name='register' value='Click Here to sign up'>
-                    </form>";
+                echo "<form method='post'>"
+                      ."Username:<br><input type='text' name='username' value='$username'><br>"
+                      ."<Password<br><input type='password' name='password'><br>"
+                      ."<input type='submit' name='login' value='login'><br>"
+                    ."</form>"
+                    ."<form method='post'>"
+                      ."<input type='submit' name='register' value='Click Here to sign up'>"
+                    ."</form>";
               }
             }
+            //else no attempt to use page has been made so display standard form
             else
             {
-              echo "
-                    <form method='post'>
-                      <input type='text' name='username'><br>
-                      <input type='password' name='password'><br>
-                      <input type='submit' name='login' value='login'>
-                    </form>
-                    <form method='post'>
-                      <input type='submit' name='register' value='Click Here to sign up'>
-                    </form>";
+              //display standard login form
+              echo "<form method='post'>"
+                      ."Username:<br><input type='text' name='username'><br>"
+                      ."Password:<br><input type='password' name='password'><br>"
+                      ."<input type='submit' name='login' value='login'><br>"
+                    ."</form>"
+                    ."<form method='post'>"
+                      ."<input type='submit' name='register' value='Click Here to sign up'>"
+                    ."</form>";
             }
           }
         ?>
       </div>
     </div>
-
-<form method='post'>
-<input type='text' name='username' value=''><br>
-<input type='password' name='password'><br>
-<input type='submit' value='login'>
-</form>
-
-
-
-
     <div class="learn-more">
       <div class="container">
         <div class="row">
