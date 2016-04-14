f6c10c200cfc0531ff51f99a783fc3aafca1f967
Ben Lister <ben.lister@student.manchester.ac.uk>
2016-04-14 13:35:53 +0100

    Report Window resize

--- Pages/ReportButton.js
+++ Pages/ReportButton.js
@@ -6,18 +6,19 @@ function reportQuestion(clickedID)
   var windowname = "Report Question";
   var leftPosition = (window.screen.width / 2) - 400;
   var topPosition = (window.screen.height / 2) - 300;
-  var myWindow = window.open(mylink, windowname, "height=700px, width=800px, top=" + topPosition + ", left=" + leftPosition + "location=no, statusbar=no, toolbar=no");
+  var myWindow = window.open(mylink, windowname, "height=750px, width=800px, top=" + topPosition + ", left=" + leftPosition + "location=no, statusbar=no, toolbar=no");
   myWindow.focus();
 }
 
 function reportModule()
+
 {
   var module = document.getElementById("moduleID").textContent;
   var mylink = "ReportModule.php?module=" + module;
   var windowname = "Report Module";
   var leftPosition = (window.screen.width / 2) - 400;
   var topPosition = (window.screen.height / 2) - 300;
-  var myWindow = window.open(mylink, windowname, "height=700px, width=800px, top=" + topPosition + ", left=" + leftPosition + "location=no, statusbar=no, toolbar=no");
+  var myWindow = window.open(mylink, windowname, "height=750px, width=800px, top=" + topPosition + ", left=" + leftPosition + "location=no, statusbar=no, toolbar=no");
   myWindow.focus();
 }
 
