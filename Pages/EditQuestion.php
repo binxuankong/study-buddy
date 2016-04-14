c0dcdb5328adfe63fdad7c9b02b8aa0bf6c4a052
Bin Xuan Kong <bin.kong@student.manchester.ac.uk>
2016-04-14 21:49:38 +0100

    edit question page now working like finally

--- Pages/EditQuestion.php
+++ Pages/EditQuestion.php
@@ -50,10 +50,98 @@
        $answers[] = $answerRow;
     }
     // SB_REPORTED_QUESTIONS
-    $result = $mysqli -> query("SELECT reportReason FROM SB_REPORTED_QUESTIONS WHERE questionID='$questionID' AND userID='$userID'");
+    $result = $mysqli -> query("SELECT * FROM SB_REPORTED_QUESTIONS WHERE questionID='$questionID' AND userID='$userID'");
     $reportedQuestionsRow = $result -> fetch_assoc();
     $reportReason = $reportedQuestionsRow['reportReason'];
-    $otherReason = $reportedQuestionsRow['otherReason'];    
+    $otherReason = $reportedQuestionsRow['otherReason'];
+
+    function test_input($data)
+    {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      $data = filter_var($data, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+      return $data;
+    }
+
+    $errorMessage = "";
+    if($_SERVER['REQUEST_METHOD'] == "POST")
+    {
+      $goodQuestion = true;
+      if(empty($_POST["question"]))
+      {
+        $errorMessage = "The question must not be empty.";
+        $goodQuestion = false;
+      }
+      else
+      {
+        $question = test_input($_POST['question']);
+        $ans = array();
+        foreach($_POST['ans'] as $newAns)
+        {
+          $ansRow = array();
+          $ansRow[] = test_input($newAns[0]);
+          if(count($newAns) == 2)
+          {
+            $ansRow[] = 1;
+          }
+          else
+          {
+            $ansRow[] = 0;
+          }
+          $ans[] = $ansRow;
+        }
+        $numberOfAnswers = 0;
+        $numberOfCorrectAnswers = 0;
+        foreach($ans as $row)
+        {
+          if($row[1] == 1 && !empty($row[0]))
+            $numberOfCorrectAnswers++;
+          if(!empty($row[0]))
+            $numberOfAnswers++;
+        }
+        $answers = array();
+        if($numberOfCorrectAnswers <= 0)
+        {
+          $errorMessage = "There must be at least one correct answer.";
+          $goodQuestion = false;
+        }
+        if($numberOfCorrectAnswers >= $numberOfAnswers)
+        {
+          $errorMessage = "The number of correct answers must be one less than the number of answer choices";
+          $goodQuestion = false;
+        }
+        foreach($ans as $row)
+        {
+          if(empty($row[0]))
+          {
+            $errorMessage = "The answer must not be empty.";
+            $goodQuestion = false;
+          }
+          $answer[] = $row;
+        }
+        if(count($answer) == 0)
+        {
+          $errorMessage = "There must be at least two answer choices.";
+          $goodQuestion = false;
+        }
+      }
+      // Save the edited question to the database.
+      if($goodQuestion)
+      {
+        $deleteAnswers = "DELETE FROM SB_ANSWERS WHERE questionID='$questionID'";
+        $mysqli->query($deleteAnswers);
+        $updateQuestion = "UPDATE SB_QUESTIONS SET questionContent='$question' WHERE questionID='$questionID'";
+        $mysqli->query($updateQuestion);
+        foreach($ans as $ansRow)
+        {
+          $sql = $mysqli -> prepare("INSERT INTO SB_ANSWERS (questionID, answerContent, answerCorrect) VALUES (?,?,?)");
+          $sql -> bind_param("sss", $questionID, $ansRow[0], $ansRow[1]);
+          $sql -> execute();
+          $sql -> close();
+        }
+      }
+    }
 
     echo "<div class='heading'>
       <div class='container'>
@@ -111,23 +199,29 @@
         echo "<li>The question/choices contain spelling error.</li>";
       }
     }
-    echo "</ul>";
+    echo "</ul>"
+         ."<span class='error'>$errorMessage</span>";
     echo "
       <form method='post'>
       <p>
       Question:<br>
-      <textarea name='question' rows='3' cols='80'>.$questionContent.</textarea><br>";
-      $answerNumber = 0;
-      foreach($answers as $answer)
-      {
-        $answerNumber = $answerNumber + 1;
-        echo "Answer ".$answerNumber." <input type='text' name='ans[0][0]' size='64' value='"
-             .$answer['answerContent']."'>";
-        if ($answer['answerCorrect'] == 1)
-          echo "<input type='checkbox' name='ans[0][1]' checked><br>";
-        else
-          echo "<input type='checkbox' name='ans[0][1]' ><br>";
-      }
+      <textarea name='question' rows='3' cols='80'>".$questionContent."</textarea><br>";
+    $answerNumber = 0;
+    $count = 0;
+    foreach($answers as $answer)
+    {
+      $answerNumber = $answerNumber + 1;
+      echo "Answer ".$answerNumber." <input type='text' name='ans[$count][0]' size='64'"
+           ."value='".$answer['answerContent']."'>";
+      if ($answer['answerCorrect'] == 1)
+        echo "<input type='checkbox' name='ans[$count][1]' value='1' checked><br>";
+      else
+        echo "<input type='checkbox' name='ans[$count][1]' ><br>";
+      $count = $count + 1;
+    }
+    echo "
+      <input type='submit' value ='Save Question'>
+      </form>";
 
   ?>
 
